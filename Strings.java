16a618e8a9953a3920ef49c64f0ca1fe8facfcce
iitjee <iit.jee.help.me@gmail.com>
2016-10-15 09:50:43 +0530

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,41 @@
+
+package java.lang.String //no need to declare explicitly
+
+String s1 = "Hey!"
+String s2 = new String("Hey");
+
+
+// Characters to String
+char[] charArray = {'A', 'E', 'I', 'O', 'U'};
+String s3 = new String(charArray);
+
+//String to Characters
+String s4 = "hello";
+char[] charArray2 = s4.toCharacterArray();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
