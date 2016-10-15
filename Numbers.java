376de3ea2cdd9d6f026eb9f60a98745cc7af9494
iitjee <iit.jee.help.me@gmail.com>
2016-10-15 09:43:16 +0530

    Create Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Booleans */
+boolean myNumber = false;
+
+//Boolean Strings
+String myBoolString = "true";
+boolean parsed = Boolean.parseBoolean(myBoolString);
+
+
+
+
+/* Characters */
+char c1 = '1'; //wrapped in single quotes
+char a1 = 'a';
+
+Character.toUpperCase(a1);
+
+
+
