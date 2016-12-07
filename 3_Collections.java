34100cab3624cc85948235ddeeee57134daa81f4
iitjee <iit.jee.help.me@gmail.com>
2016-12-07 20:34:57 +0530

    Create 3_Collections.java

---
+++ 3_Collections.java
@@ -0,0 +1,20 @@
+/*
+  Note: ints are initialized to 0 by default and boolean to false.
+
+*/
+
+int[] ints = new int[10]
+int[] ints = {1, 2, 3}
+
+String[][] states = new String[3][2]
+states[0][0] = "California"
+states[0][1] = "Sacramentor"
+
+//Common methods used:
+myArray.length  //length of arr
+Arrays.sort(myInts) //sort number array
+System.arraycopy(myArr, 5, destinArry, 0, 5) //Copying array 2nd arg=first 5 elems of myArr 4th&5th args = first and last indices of destinArryy
+
+
+
+
