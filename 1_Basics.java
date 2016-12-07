23c25551f8e63661a40ff4aa29a79e29eb34dd71
iitjee <iit.jee.help.me@gmail.com>
2016-12-07 19:53:34 +0530

    Create 1_Basics.java

---
+++ 1_Basics.java
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+  }
+}
+
+/*  Running Code  */
+The default class file to be run is the main file. Run -> First run (^R)
+If you want another class file to be run : Run -> 4th option (^ + OPT +R)
+To debug, You've to run in "debug" mode
+
+
+/* passing by value and passing by reference  */
+All primitive types are passed by value. 
+
+
+
+
